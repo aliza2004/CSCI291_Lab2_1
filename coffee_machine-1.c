5f84e033debc55a2b7a9f27c733a4da6266a82ee
aliza2004 <alizaaligcse@gmail.com>
2024-09-27 12:22:42 +0400

    Add files via upload

---
+++ coffee_machine-1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+#define ESPRESSO_BEANS 8 //grams
+#define ESPRESSO_WATER 30 //milliliters
+
+#define CAPPUCCINO_BEANS 8 //grams
+#define CAPPUCCINO_WATER 30 //milliliters
+#define CAPPUCCINO_MILK 70 //milliliters
+
+#define MOCHA_BEANS 8 //grams
+#define MOCHA_WATER 30 //milliliters
+#define MOCHA_MILK 70 //milliliters
+#define MOCHA_SYRUP 30 //milliliters
+
+#define PRICE_ESPRESSO 3.5 //AED
+#define PRICE_CAPPUCCINO 4.5 //AED
+#define PRICE_MOCHA 5.5 //AED
+
+
+
+
+
+
+
+
+
+
+    
+}
